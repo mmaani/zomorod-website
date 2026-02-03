b88486022f6622f6949dff8803fb31d177bcc77e
Mohammad Maani <mohammad.maani@hotmail.com>
2026-02-03 05:07:41 +0000

    temp: force clear service workers and caches

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+// TEMP: force-remove any old SW / cache on client devices
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((r) => r.unregister());
+  });
+}
+
+if ("caches" in window) {
+  caches.keys().then((keys) => keys.forEach((k) => caches.delete(k)));
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
